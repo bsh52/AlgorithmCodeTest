f05b66f676c551f3551e726f16f703f958815325
SH Baek <90443796+paek1127@users.noreply.github.com>
2024-12-13 17:01:09 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ Java/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        int num = 0;
-        int order = 0;
         for (int i = 0; i < array.length; i++) {
-            if (num < array[i]) {
-                num = array[i];
-                order = i;
+            if (answer[0] < array[i]) {
+                answer[0] = array[i];
+                answer[1] = i;
             }
         }
-        answer[0] = num;
-        answer[1] = order;
         
         return answer;
     }
