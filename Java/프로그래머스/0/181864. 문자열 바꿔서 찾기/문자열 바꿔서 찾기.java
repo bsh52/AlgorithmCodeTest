07c43e6e870b61814065750d7c66b05c5b51f9cb
SH Baek <90443796+bsh52@users.noreply.github.com>
2024-12-30 15:20:02 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.replace("A", "C").replace("B", "A").replace("C", "B");
+        int answer = myString.contains(pat) ? 1 : 0;
+        return answer;
+    }
+}
