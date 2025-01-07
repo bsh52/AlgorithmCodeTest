1a9e176b9f87bc4e9e5e137547eec8ef3df348b7
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-01-08 08:46:19 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+        
+        for (int i : num_list) {
+            if (i % 2 == 0) {
+                even *= 10;
+                even += i;
+            } else {
+                odd *= 10;
+                odd += i;
+            }
+        }
+        
+        return even + odd;
+    }
+}
