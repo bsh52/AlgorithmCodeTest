c4f2b6dcf3a6eccbb9d68b963ba152be3c328768
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-01-09 17:33:33 +0900

    [level 1] Title: 하샤드 수, Time: 2.03 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public boolean solution(int x) {
+        int sum = Arrays.stream(String.valueOf(x).split("")).mapToInt(Integer::parseInt).sum();
+        return x % sum == 0;
+    }
+}
