99c5b4d1b4b97c91ea7038ce4b6906ceb79c394f
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-02-04 09:19:49 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.48 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        int[] sort = Arrays.copyOf(emergency, emergency.length);
+        Arrays.sort(sort);
+        Map<Integer, Integer> map = new HashMap<>();
+        int idx = 1;
+        
+        for (int i = sort.length - 1; i >= 0; i--) {
+            map.put(sort[i], idx++);
+        }
+        
+        for (int i = 0; i < emergency.length; i++) {
+            answer[i] = map.get(emergency[i]);
+        }
+        return answer;
+    }
+}
