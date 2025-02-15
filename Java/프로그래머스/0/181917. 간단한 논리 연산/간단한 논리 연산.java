1173bf1ac49ab3ae9be6f4ce3b4285efb99203c7
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-02-15 09:36:37 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 84 MB -BaekjoonHub

--- Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = and((or(x1, x2)), (or(x3, x4)));
+        boolean answer = (x1||x2) && (x3||x4);
         return answer;
-    }
-    
-    public boolean or (boolean a, boolean b) {
-        if (a || b) return true; else return false;
-    }
-    
-    public boolean and (boolean a, boolean b) {
-        if (a && b) return true; else return false;
     }
 }
