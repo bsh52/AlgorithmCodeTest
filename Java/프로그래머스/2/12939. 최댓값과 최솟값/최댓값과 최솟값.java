be920e4c3a43005dde34a21fa55d1bd656da258d
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-04-11 02:14:35 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.64 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, Integer.parseInt(arr[i]));
+            min = Math.min(min, Integer.parseInt(arr[i]));
+        }
+        answer = min + " " + max;
+        return answer;
+    }
+}
