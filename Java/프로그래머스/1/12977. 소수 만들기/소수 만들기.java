32a1408d1719930ef6cb633b34ee3ac59edb4c01
SH Baek <90443796+bsh52@users.noreply.github.com>
2025-05-27 14:44:21 +0900

    [level 1] Title: 소수 만들기, Time: 1.72 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] nums) {
+        int n = nums.length;
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    
+    private static boolean isPrime(int num) {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
