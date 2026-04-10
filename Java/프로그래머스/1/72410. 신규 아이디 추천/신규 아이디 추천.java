74934507dc7bd21306efc703bdd3f7c7bfdc15e6
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-04-10 13:33:02 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.06 ms, Memory: 75 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9._-]", "");
+        answer = answer.replaceAll("\\.+", ".");
+        if (answer.startsWith(".")) {
+            answer = answer.substring(1);
+        }
+        if (answer.endsWith(".")) {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        if (answer.isBlank()) {
+            answer = "a";
+        }
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            if (answer.endsWith(".")) {
+                answer = answer.substring(0, answer.length() - 1);
+            }
+        }
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
+            }
+        }
+        return answer;
+    }
+}
