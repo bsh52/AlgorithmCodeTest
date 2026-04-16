0cb2c9aec3e12eecd62324ae9f591e44523c8434
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-04-16 18:42:44 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        for (int value : map.values()) {
+            answer *= (value + 1);
+        }
+        answer--;
+
+        return answer;
+    }
+}
