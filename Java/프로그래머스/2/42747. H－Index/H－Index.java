82fd45dc7a749a9ca78d106fa68237c0bba80309
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-04-16 19:51:00 +0900

    [level 2] Title: H-Index, Time: 1.54 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length / 2; i++) {
+            int temp = citations[i];
+            citations[i] = citations[citations.length - 1 - i];
+            citations[citations.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] < i + 1) {
+                answer = i;
+                break;
+            }
+            answer = citations.length;
+        }
+        return answer;
+    }
+}
