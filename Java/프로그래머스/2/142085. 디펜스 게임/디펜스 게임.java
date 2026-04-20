a1fbecd3de200be144ccf98a20c55ec3c5849390
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-04-20 17:25:44 +0900

    [level 2] Title: 디펜스 게임, Time: 103.95 ms, Memory: 136 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < enemy.length; i++) {
+            n -= enemy[i];
+            queue.add(enemy[i]);
+
+            if (n < 0) {
+                if (k > 0) {
+                    n += queue.poll();
+                    k--;
+                } else {
+                    break;
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
