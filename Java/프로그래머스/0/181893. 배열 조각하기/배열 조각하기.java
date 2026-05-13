3c9e77c457cca9834993c810e846a14199e1e022
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-05-13 17:28:54 +0900

    [level 0] Title: 배열 조각하기, Time: 4.00 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr) {
+            list.add(i);
+        }
+        
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                list = list.subList(0, query[i] + 1);
+            } else {
+                list = list.subList(query[i], list.size());
+            }
+        }
+
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
