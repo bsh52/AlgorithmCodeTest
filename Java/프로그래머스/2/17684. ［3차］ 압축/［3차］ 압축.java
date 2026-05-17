9e00d314bfb20dca1429bea3b7d66804ab6d29d5
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-05-17 15:32:04 +0900

    [level 2] Title: [3차] 압축, Time: 18.00 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 1; i <= 26; i++) {
+            map.put(String.valueOf((char)('A' + i - 1)), i);
+        }
+
+        int idx = 0;
+
+        while (idx < msg.length()) {
+            String key = "";
+            while (idx < msg.length()) {
+                if (!map.containsKey(key + msg.charAt(idx))) {
+                    break;
+                } else {
+                    key += msg.charAt(idx);
+                }
+                idx++;
+            }
+            answer.add(map.get(key));
+
+            if (idx < msg.length()) {
+                map.put(key + msg.charAt(idx), map.size() + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
