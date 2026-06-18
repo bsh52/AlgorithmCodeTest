2264c42b2b1c30dd2f07b7b2a8927c12706776d5
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-06-18 17:56:41 +0900

    [level 4] Title: 단어 퍼즐, Time: 20.12 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] strs, String t) {
+        int[] dp = new int[t.length() + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int i = 0; i < t.length(); i++) {
+            for (String str : strs) {
+                if (t.startsWith(str, i)) {
+                    dp[i + str.length()] = Math.min(dp[i + str.length()], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[t.length()] == Integer.MAX_VALUE - 1 ? -1 : dp[t.length()];
+    }
+}
