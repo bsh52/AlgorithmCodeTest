62424704f8f4b4f7a5ec3f80f5185410caa62bb9
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-03 11:26:10 +0900

    [level 1] Title: K번째수, Time: 3.36 ms, Memory: 74.3 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,17 +3,11 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] result = new int[commands.length];
-        for (int[] command : commands) {
-            int i = command[0];
-            int j = command[1];
-            int k = command[2];
-
-            int[] subArray = new int[j - i + 1];
-            for (int m = i - 1; m < j; m++) {
-                subArray[m - (i - 1)] = array[m];
-            }
-            Arrays.sort(subArray);
-            result[Arrays.asList(commands).indexOf(command)] = subArray[k - 1];
+        for (int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int[] arr = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(arr);
+            result[i] = arr[command[2] - 1];
         }
         return result;
     }
