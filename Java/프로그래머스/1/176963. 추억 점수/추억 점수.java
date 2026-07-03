04673c517afcc0297aab7c25d0b8cab48c002fb8
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-03 14:12:56 +0900

    [level 1] Title: 추억 점수, Time: 1.20 ms, Memory: 87.8 MB -BaekjoonHub

--- Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        for (int i = 0; i < photo.length; i++) {
-            for (int j = 0; j < photo[i].length; j++) {
-                for (int k = 0; k < name.length; k++) {
-                    if (photo[i][j].equals(name[k])) {
-                        answer[i] += yearning[k];
-                    }
-                }
-            }
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
         }
+
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                sum += map.getOrDefault(photo[i][j], 0);
+            }
+            answer[i] = sum;
+        }
+
         return answer;
     }
 }
