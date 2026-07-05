9d9ed2cbc6bff1143b4a1c84c02e3a8f393546e4
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-05 15:03:05 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.02 ms, Memory: 59.8 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(x: Int, n: Int): LongArray {
+        val answer = LongArray(n)
+        for (i in 0 until n) {
+            answer[i] = (i + 1L) * x
+        }
+        return answer
+    }
+}
