26ef344ee655782d1f02fb05a2aa7919c26eeafb
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-06 14:43:50 +0900

    [level 1] Title: 기사단원의 무기, Time: 49.82 ms, Memory: 78.8 MB -BaekjoonHub

--- Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,21 +1,23 @@
+import java.util.*;
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            int sum = 0;
-            for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) {
-                    sum++;
-                } else if (i % j == 0) {
-                    sum += 2;
-                }
-            }
-            if (sum > limit) {
-                answer += power;
-            } else {
-                answer += sum;
-            }
+        int[] divisor = getDivisor(number);
+        for (int i = 0; i < divisor.length; i++) {
+            if (divisor[i] > limit) divisor[i] = power;
         }
-        return answer;
+        return Arrays.stream(divisor).sum();
+    }
+
+    private int[] getDivisor(int number) {
+        int[] divisor = new int[number];
+        for (int i = 1; i <= number; i++) {
+            int div = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) div++;
+                else if (i % j == 0) div += 2;
+            }
+            divisor[i - 1] = div;
+        }
+        return divisor;
     }
 }
