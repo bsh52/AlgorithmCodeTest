435c24edd8d9cb0db46d7e9f824a22aee14eb9a7
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-06 11:37:13 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.78 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(strings: Array<String>, n: Int): Array<String> {
+        strings.sortWith(compareBy<String> { it[n] }.thenBy { it })
+        return strings
+    }
+}
