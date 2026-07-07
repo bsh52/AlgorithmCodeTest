314eb0a6c49796cb12df77548e9bc38ddba54447
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-07-07 22:36:46 +0900

    [level 2] Title: 피보나치 수, Time: 1.68 ms, Memory: 75.1 MB -BaekjoonHub

--- Java/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ Java/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = fibo(n);
-        return answer;
-    }
-
-    int fibo(int n) {
-        int[] arr = new int[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
+        int[] fibo = new int[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
         for (int i = 2; i <= n; i++) {
-            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
         }
-        return arr[n];
+
+        return fibo[n];
     }
 }
