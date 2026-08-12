8fc49193aa6f7ff24f40211674b2ffbddfd34e3e
SH Baek <90443796+bsh52@users.noreply.github.com>
2026-08-12 17:09:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = ""
+    arr = s.split(" ")
+    for i in range(len(arr)):
+        arr[i] = arr[i][:1].upper() + arr[i][1:].lower()
+    answer = " ".join(arr)
+
+    return answer
